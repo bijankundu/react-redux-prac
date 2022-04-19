85c1b7ee1646bdd5d39b9ef0d11386f9a6de6faa
Bijan Kundu <bijan.kundu@practo.com>
2022-04-19 15:02:53 +0530

    added go to cart button on homepage

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 import MenuItemCard from "../../components/MenuItemCard";
 
@@ -7,6 +9,9 @@ import styles from "./home.module.scss";
 import samplMenuData from "./../../sampleData.json";
 
 const Home = () => {
+  const totalQty = useSelector((state) => state.cartItems.totalQty);
+  const navigate = useNavigate();
+
   return (
     <main className={styles.container}>
       <section className={styles["food-card-container"]}>
@@ -14,6 +19,13 @@ const Home = () => {
           return <MenuItemCard key={item.id} itemDetails={item} />;
         })}
       </section>
+
+      {totalQty > 0 && (
+        <div className={styles["bottom-fixed-container"]}>
+          <p>{`Total Items : ${totalQty}`}</p>
+          <button onClick={() => navigate("/checkout")}>Go to cart</button>
+        </div>
+      )}
     </main>
   );
 };
